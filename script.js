cad307ff1157fee83a118fe4094cdcf99e06abf3
YUSUF-ALSHIEKH <yousifsheikh19@gmail.com>
2026-02-09 11:18:04 +0300

    create start_page and start test on js

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-console.log("hi all")
+const pieceNumber = document.querySelector("#info")
+const green_Pieces = document.querySelector("#g1")
+const up_Down_Cells = document.getElementById("1")
+const startButton = document.querySelector("#sumbit")
+
+class Plays_Pieces {
+  constructor(numbers, color, players) {
+    this.number = numbers
+    this.color = color
+    this.players = players
+  }
+}
+function init() {
+  const pieceNum = pieceNumber.Value()
+  console.log(pieceNum)
+}
+const pieceNum = pieceNumber.Value
+console.log(pieceNum)
+
+startButton.addEventListener("click", init)
+// startButton.addEventListener("click", takeinfo)
